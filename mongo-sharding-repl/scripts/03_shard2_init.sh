894e499bb24cf1ac600f8d7d36e6889a175fa247
EvgenyMaklakov <maklakov.eugene@gmail.com>
2025-03-02 17:17:49 +0300

    Task 3

--- mongo-sharding-repl/scripts/03_shard2_init.sh
+++ mongo-sharding-repl/scripts/03_shard2_init.sh
@@ -2,12 +2,14 @@
 
 echo "Start second shard init"
 
-docker compose exec -T shard2 mongosh --port 27019 --quiet <<EOF
+docker compose exec -T shard2_1 mongosh --port 27019 --quiet <<EOF
 rs.initiate(
     {
       _id : "shard2",
       members: [
-        { _id : 1, host : "shard2:27019" }
+        { _id : 0, host : "shard2_1:27019" },
+        { _id : 1, host : "shard2_2:27023" },
+        { _id : 2, host : "shard2_3:27024" }
       ]
     }
 );
--- mongo-sharding-repl/scripts/04_router_init.sh
+++ mongo-sharding-repl/scripts/04_router_init.sh
@@ -3,8 +3,8 @@
 echo "Start router init"
 
 docker compose exec -T mongos_router mongosh --port 27020 --quiet <<EOF
-sh.addShard( "shard1/shard1:27018")
-sh.addShard( "shard2/shard2:27019")
+sh.addShard( "shard1/shard1_1:27018")
+sh.addShard( "shard2/shard2_1:27019")
 
 sh.enableSharding("somedb")
 sh.shardCollection("somedb.helloDoc", { "name" : "hashed" } )
